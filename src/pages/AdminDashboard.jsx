6663f569aaea42f8a2d6db957c63a111c526689a
Almas012 <a40018242@gmail.com>
2026-02-18 10:46:45 +0530

    made sidebar sticky

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -36,7 +36,7 @@ const AdminDashboard = () => {
   };
 
   return (
-    <div className="flex min-h-screen bg-slate-50">
+    <div className="flex h-screen bg-slate-50 overflow-hidden">
       {/* Sidebar */}
       <motion.aside 
         initial={{ width: isSidebarOpen ? 250 : 0, opacity: isSidebarOpen ? 1 : 0 }}
